6aefed0796e6a0737b98d04636ae66e93eae4d10
reshmi-02 <reshmihash123@gmail.com>
2023-10-16 04:45:31 -0700

    addnew

---
+++ main.js
@@ -0,0 +1,27 @@
+
+var a=parseInt(prompt("Enter the day in number"));
+
+if(a==1){
+    document.write("Monday");
+}
+else if(a==2){
+  document.write("Tuesday");
+}
+else if(a==3){
+  document.write("Wednesday");
+}
+else if(a==4){
+  document.write("Thursday");
+}
+else if(a==5){
+  document.write("Friday");
+}
+else if(a==6){
+  document.write("Saturday");
+}
+else if(a==7){
+  document.write("Sunday");
+}
+else{
+  document.write("enter a valid number");
+}
